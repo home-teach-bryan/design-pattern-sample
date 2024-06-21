01a5f5058fc6a0400aadb015696047fb21d9df9a
bryan.yu <bryan.mj.yu@tripresso.com>
2024-06-21 14:13:52 +0800

    Singleton Pattern

---
+++ DesignPatternSample/DesignPatternSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Channels;
+
+namespace DesignPatternSample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region 單例模式(Singleton)
+
+        var firstSingleton = Singleton.Singleton.Instance;
+        Console.WriteLine(firstSingleton.GetId());
+        var secondSingleton = Singleton.Singleton.Instance;
+        Console.WriteLine(secondSingleton.GetId());
+
+        #endregion
+        
+        
+        
+        Console.Read();
+    }
+}
---
+++ DesignPatternSample/DesignPatternSample/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DesignPatternSample.Singleton;
+
+public sealed class Singleton
+{
+    private static Singleton _singleton = new Singleton();
+
+    private readonly int _id;
+
+    private Singleton()
+    {
+        this._id = Guid.NewGuid().GetHashCode();
+    }
+
+    public static Singleton Instance
+    {
+        get { return _singleton; }
+    }
+
+    public int GetId()
+    {
+        return this._id;
+    }
+}
